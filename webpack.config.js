ee103f81f49c36ae5e22c0ed2755d0bca01b4248
Taras Shedenko <taras.shedenko@gmail.com>
2020-12-13 17:19:07 +0200

    Adds tests

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,28 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+/*eslint-env node */
+/*eslint-disable @typescript-eslint/no-var-requires */
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const htmlWebpackPlugin = new HtmlWebpackPlugin({ template: './src/index.html' });
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+  template: "./src/index.html",
+});
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.tsx',
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?/,
-                loader: 'ts-loader'
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.js', '.ts', '.tsx']
-    },
-    plugins: [htmlWebpackPlugin]
+  mode: "development",
+  entry: "./src/index.tsx",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?/,
+        loader: "ts-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".tsx"],
+  },
+  plugins: [htmlWebpackPlugin],
 };
